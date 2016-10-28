9b908d6872f96ce4a0c844d6f197e0f1beba8fef
Wojciech <wporoslo@gmail.com>
2016-10-28 21:43:31 +0200

    introduced first junit test in my life

---
+++ src/main/java/com/github/wporoslo/javanoob/MathOperations.java
@@ -0,0 +1,10 @@
+package com.github.wporoslo.javanoob;
+
+/**
+ * Created by Wojciech on 28.10.2016.
+ */
+public class MathOperations {
+    public int sum(int termOne, int termTwo) {
+        return termOne + termTwo;
+    }
+}
---
+++ src/test/java/com/github/wporoslo/javanoob/MathOperationsTest.java
@@ -0,0 +1,19 @@
+package com.github.wporoslo.javanoob;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ * Created by Wojciech on 28.10.2016.
+ */
+public class MathOperationsTest {
+    @Test
+    public void testAdd() throws Exception {
+        MathOperations operations = new MathOperations();
+        int sum = operations.sum(2, 3);
+        Assert.assertThat(sum, is(5));
+    }
+}
+
