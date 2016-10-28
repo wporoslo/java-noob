54e055a5eede1b5a5c2f68dd9bcad381d9830ffc
Wojciech <wporoslo@gmail.com>
2016-10-28 20:08:21 +0200

    initial commit

---
+++ src/main/java/com/github/wporoslo/javanoob/JavaNoob.java
@@ -0,0 +1,16 @@
+package com.github.wporoslo.javanoob;
+
+/**
+ * Created by Wojciech on 28.10.2016.
+ */
+public class JavaNoob {
+    public static void main(String[] args) {
+        JavaNoob.printHello("wojtek");
+    }
+
+    static void printHello(String name) {
+        System.out.println("Hello, " + name + "!");
+        System.out.println("Bye bye, " + name + "!");
+    }
+
+}
