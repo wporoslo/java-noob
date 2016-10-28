f95c99b037fee30c5e4dac8c13bd718a95c78505
Wojciech <wporoslo@gmail.com>
2016-10-28 20:47:43 +0200

    added readName() method

--- src/main/java/com/github/wporoslo/javanoob/JavaNoob.java
+++ src/main/java/com/github/wporoslo/javanoob/JavaNoob.java
@@ -1,16 +1,31 @@
 package com.github.wporoslo.javanoob;
 
+import java.util.Scanner;
+
 /**
  * Created by Wojciech on 28.10.2016.
  */
 public class JavaNoob {
     public static void main(String[] args) {
-        JavaNoob.printHello("wojtek");
+        String name = readName();
+        JavaNoob.printHello(name);
     }
 
     static void printHello(String name) {
         System.out.println("Hello, " + name + "!");
         System.out.println("Bye bye, " + name + "!");
+    }
+
+    static String readName() { //tworzę static metodę zwracającą String
+        //tworzę zmienną lokalną typu Scanner (klasa interpretująca znaki)
+        // (System.in reprezentuje standardowe wejście (STD IN), np. klawiature)
+        Scanner scanner = new Scanner(System.in);
+        // calluję metodę do wypisania znaków na STD OUT
+        System.out.print("Enter your name: ");
+        // tworzę zmienną name typu String i przypisuję do niej wartość wykonania metody scanner.nextLine()
+        // scanner.nextLine() odczytuje ostatnią wprowadzoną linię ze swojego źródła (czyli System.in - klawiatura)
+        String name = scanner.nextLine();
+        return name;
     }
 
 }
