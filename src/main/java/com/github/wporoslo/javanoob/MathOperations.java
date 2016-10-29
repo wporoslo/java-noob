046d858b78497355bc88f01a68acc4ae09939a58
Wojciech <wporoslo@gmail.com>
2016-10-29 20:59:02 +0200

    added 4 basic math operations: subtraction, division, multiplication, exponentiation

--- src/main/java/com/github/wporoslo/javanoob/MathOperations.java
+++ src/main/java/com/github/wporoslo/javanoob/MathOperations.java
@@ -7,4 +7,24 @@ public class MathOperations {
     public int sum(int termOne, int termTwo) {
         return termOne + termTwo;
     }
+
+    public int difference(int termOne, int termTwo) {
+        return termOne - termTwo;
+    }
+
+    public int product(int termOne, int termTwo) {
+        return termOne * termTwo;
+    }
+
+    public int quotient(int dividend, int divisor) {
+        return dividend / divisor;
+    }
+
+    public int power(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+        return power(base, exponent - 1) * base;
+    }
+
 }
--- src/test/java/com/github/wporoslo/javanoob/MathOperationsTest.java
+++ src/test/java/com/github/wporoslo/javanoob/MathOperationsTest.java
@@ -18,7 +18,30 @@ public class MathOperationsTest {
 
     @Test
     public void testSubtract() throws Exception {
+        MathOperations operations = new MathOperations();
+        int difference = operations.difference(5, 3);
+        Assert.assertThat(difference, is(2));
+    }
 
+    @Test
+    public void testMultiply() throws Exception {
+        MathOperations operations = new MathOperations();
+        int product = operations.product(2, 3);
+        Assert.assertThat(product, is(6));
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+        MathOperations operations = new MathOperations();
+        int quotient = operations.quotient(8, 2);
+        Assert.assertThat(quotient, is(4));
+    }
+
+    @Test
+    public void testExponentiate() throws Exception {
+        MathOperations operations = new MathOperations();
+        int power = operations.power(3, 3);
+        Assert.assertThat(power, is(27));
     }
 }
 
